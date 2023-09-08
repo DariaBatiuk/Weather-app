38311427ef9be7616c08d628cad80714a87fd14a
Daria <dariabatiuk5@gmail.com>
2023-09-08 00:24:12 -0300

    fetching data on the screen

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -11,7 +11,18 @@ import axios from 'axios';
 interface WeatherDataProps{
 	name: string;
 	main: {
-		temp: string
+		temp: number;
+		humidity: number;
+	},
+	sys:{
+		country: string;
+	},
+	weather: {
+		main: string;
+		icon: string;
+	}[],
+	wind: {
+		speed: number;
 	}
 }
 
@@ -21,13 +32,29 @@ const DisplayWeather = () => {
 	const API_CALL = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
 	const API_Endpoint = 'https://api.openweathermap.org/data/2.5/';
 
-	const [weatherData, setWeatherData] = React.useState();
+	const [weatherData, setWeatherData] = React.useState<WeatherDataProps | null> (null);
 
 	const fetchCurrentWeather = async (lat:number, lon:number) => {
 		const url = `${API_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
 		const response = await axios.get(url);
 		return response.data;
+	};
+
+	const iconChange = (weather:string) =>{
+		let iconElement: React.ReactNode;
+		let iconColor: string;
+
+		switch(weather){
+			case "Rain":
+			iconElement = <BsFillCloudRainFill />;
+			iconColor = "#272829";
+			break;
+
+			default:
+				iconElement = <TiWeatherPartlySunny />
+				iconColor='#7B2869'
+		}
 	}
 
 	useEffect(() => {
@@ -36,6 +63,8 @@ const DisplayWeather = () => {
       try {
         const currentWeather = await fetchCurrentWeather(latitude, longitude);
         setWeatherData(currentWeather);
+				console.log(currentWeather);
+
       } catch (error) {
         console.error('Error fetching weather data:', error);
       }
@@ -52,31 +81,38 @@ const DisplayWeather = () => {
 						<AiOutlineSearch className='searchIcon'/>
 					</div>
 				</div>
-				<div className="weatherArea">
-					<h1> Moscow </h1>
-					<span> Russia </span>
-					<div className='icon '>icon</div>
-					<h2>18C</h2>
-					<h2>Cloudy</h2>
+
+				{
+					weatherData && (
+						<>
+											<div className="weatherArea">
+					<h1> {weatherData.name} </h1>
+					<span> {weatherData.sys.country} </span>
+					<div className='icon '>{weatherData.weather[0].icon}</div>
+					<h2>{weatherData.main.temp}</h2>
+					<h2>{weatherData.weather[0].main}</h2>
 				</div>
 				<div className="bottomInfoArea">
 					<div className="humidity">
 						<WiHumidity className='windIcon'/>
 						<div className='humiInfo'>
-							<h2>60%</h2>
+							<h2>{weatherData.main.humidity}</h2>
 							<p>Humidity</p>
 						</div>
 					</div>
 					<div className="wind">
 						<SiWindicss className="windIcon"/>
 						<div className='windInfo'>
-							<h2>2.35km
-								h
+							<h2>{weatherData.wind.speed}
 							</h2>
 							<p>Wind speed</p>
 						</div>
 					</div>
 				</div>
+						</>
+					)
+				}
+
 			</div>
 		</MainWrapper>
 	)
